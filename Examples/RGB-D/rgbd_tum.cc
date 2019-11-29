52335603a0730a3045c5d78d3cdec04d8097cb51
linmeeka <461884867@qq.com>
2019-11-29 14:32:01 +0800

    semantic map and polar check

---
+++ Examples/RGB-D/rgbd_tum.cc
@@ -0,0 +1,290 @@
+/**
+* This file is a modified version of ORB-SLAM2.<https://github.com/raulmur/ORB_SLAM2>
+*
+* This file is part of DynaSLAM.
+* Copyright (C) 2018 Berta Bescos <bbescos at unizar dot es> (University of Zaragoza)
+* For more information see <https://github.com/bertabescos/DynaSLAM>.
+*
+*/
+
+
+#include<iostream>
+#include<algorithm>
+#include<fstream>
+#include<chrono>
+#include <unistd.h>
+#include<opencv2/core/core.hpp>
+
+#include "Geometry.h"
+#include "MaskNet.h"
+#include <System.h>
+
+using namespace std;
+
+void LoadImages(const string &strAssociationFilename, vector<string> &vstrImageFilenamesRGB,
+                vector<string> &vstrImageFilenamesD, vector<double> &vTimestamps);
+
+void getMask(const cv::Mat & maskRCNN, cv::Mat &mask, cv::Mat &maskColor);
+
+void print(cv::Mat &mask)
+{
+     for(int i=0;i<mask.rows;i++)
+    {
+        for(int j=0;j<mask.cols;j++)
+        {
+            if(mask.channels()==1)
+                cout<<mask.at<uchar>(i,j)<<" ";
+            else
+            {
+                cout<<mask.at<cv::Vec3b>(i,j)[0]<<",";
+                cout<<mask.at<cv::Vec3b>(i,j)[1]<<",";
+                cout<<mask.at<cv::Vec3b>(i,j)[2]<<",";
+                cout<<" ";
+                
+            }
+        }
+        cout<<endl;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    if(argc != 5 && argc != 6 && argc != 7)
+    {
+        cerr << endl << "Usage: ./rgbd_tum path_to_vocabulary path_to_settings path_to_sequence path_to_association (path_to_masks) (path_to_output)" << endl;
+        return 1;
+    }
+
+    // Retrieve paths to images
+    vector<string> vstrImageFilenamesRGB;
+    vector<string> vstrImageFilenamesD;
+    vector<double> vTimestamps;
+    string strAssociationFilename = string(argv[4]);
+    LoadImages(strAssociationFilename, vstrImageFilenamesRGB, vstrImageFilenamesD, vTimestamps);
+
+    // Check consistency in the number of images and depthmaps
+    int nImages = vstrImageFilenamesRGB.size();
+    std::cout << "nImages: " << nImages << std::endl;
+
+    if(vstrImageFilenamesRGB.empty())
+    {
+        cerr << endl << "No images found in provided path." << endl;
+        return 1;
+    }
+    else if(vstrImageFilenamesD.size()!=vstrImageFilenamesRGB.size())
+    {
+        cerr << endl << "Different number of images for rgb and depth." << endl;
+        return 1;
+    }
+
+    // Initialize Mask R-CNN
+    DynaSLAM::SegmentDynObject *MaskNet;
+    if (argc==6 || argc==7)
+    {
+        cout << "Loading Mask R-CNN. This could take a while..." << endl;
+        MaskNet = new DynaSLAM::SegmentDynObject();
+        cout << "Mask R-CNN loaded!" << endl;
+    }
+
+    // Create SLAM system. It initializes all system threads and gets ready to process frames.
+    ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::RGBD,true);
+
+    // Vector for tracking time statistics
+    vector<float> vTimesTrack;
+    vTimesTrack.resize(nImages);
+
+    cout << endl << "-------" << endl;
+    cout << "Start processing sequence ..." << endl;
+    cout << "Images in the sequence: " << nImages << endl << endl;
+
+    // Dilation settings
+    int dilation_size = 15;
+    cv::Mat kernel = getStructuringElement(cv::MORPH_ELLIPSE,
+                                           cv::Size( 2*dilation_size + 1, 2*dilation_size+1 ),
+                                           cv::Point( dilation_size, dilation_size ) );
+
+    if (argc==7)
+    {
+        std::string dir = string(argv[6]);
+        mkdir(dir.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+        dir = string(argv[6]) + "/rgb/";
+        mkdir(dir.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+        dir = string(argv[6]) + "/depth/";
+        mkdir(dir.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+        dir = string(argv[6]) + "/mask/";
+        mkdir(dir.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    }
+
+    // Main loop
+        cv::Mat imRGB, imD;
+        cv::Mat imRGBOut, imDOut,maskOut;
+
+    for(int ni=0; ni<nImages; ni++)
+    {
+        // Read image and depthmap from file
+        imRGB = cv::imread(string(argv[3])+"/"+vstrImageFilenamesRGB[ni],CV_LOAD_IMAGE_UNCHANGED);
+        //cout<<"channels: "<<imRGB.channels()<<endl;
+        imD = cv::imread(string(argv[3])+"/"+vstrImageFilenamesD[ni],CV_LOAD_IMAGE_UNCHANGED);
+        //cout<<"channels: "<<imD.channels()<<endl;
+        double tframe = vTimestamps[ni];
+
+        if(imRGB.empty())
+        {
+            cerr << endl << "Failed to load image at: "
+                 << string(argv[3]) << "/" << vstrImageFilenamesRGB[ni] << endl;
+            return 1;
+        }
+
+#ifdef COMPILEDWITHC11
+        std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+#else
+        std::chrono::monotonic_clock::time_point t1 = std::chrono::monotonic_clock::now();
+#endif
+
+        // Segment out the images
+        //cv::Mat mask = cv::Mat::ones(480,640,CV_8U);
+        cv::Mat mask,maskColor;
+        if (argc == 6 || argc == 7)
+        {
+            cv::Mat maskRCNN;
+            maskRCNN = MaskNet->GetSegmentation(imRGB,string(argv[5]),vstrImageFilenamesRGB[ni].replace(0,4,""));
+            // cv::Mat maskRCNNdil = maskRCNN.clone();
+            // cv::dilate(maskRCNN,maskRCNNdil, kernel);
+            // mask = mask - maskRCNNdil;
+            //cv::Mat mask,maskColor;
+            //mask = cv::Mat::ones(480,640,CV_8U);
+            mask=maskRCNN.clone();
+            maskColor=imRGB.clone();
+            getMask(maskRCNN,mask,maskColor);
+            //print(mask);
+            //print(maskColor);
+            //imwrite("mask.jpg",mask);
+            //imwrite("maskcolor.jpg",maskColor);
+            cv::Mat maskRCNNdil = mask.clone();
+            //cv::dilate(mask,maskRCNNdil, kernel);
+            cv::dilate(maskRCNN,maskRCNNdil, kernel);
+            //imwrite("maskRCNNdil.jpg",maskRCNNdil);
+            cv::Mat maskTmp = cv::Mat::ones(480,640,CV_8U);
+            mask = maskTmp - maskRCNNdil;
+           // imwrite("maskt.jpg",mask);
+        }
+
+        // Pass the image to the SLAM system
+        //if (argc == 7){SLAM.TrackRGBD(imRGB,imD,mask,tframe,imRGBOut,imDOut,maskOut);}
+        if (argc == 7){
+           
+            SLAM.TrackRGBD(imRGB,imD,mask,maskColor,tframe,imRGBOut,imDOut,maskOut);
+            }
+        else {SLAM.TrackRGBD(imRGB,imD,mask,tframe);}
+
+#ifdef COMPILEDWITHC11
+        std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+#else
+        std::chrono::monotonic_clock::time_point t2 = std::chrono::monotonic_clock::now();
+#endif
+
+        if (argc == 7)
+        {
+            cv::imwrite(string(argv[6]) + "/rgb/" + vstrImageFilenamesRGB[ni],imRGBOut);
+            vstrImageFilenamesD[ni].replace(0,6,"");
+            cv::imwrite(string(argv[6]) + "/depth/" + vstrImageFilenamesD[ni],imDOut);
+            cv::imwrite(string(argv[6]) + "/mask/" + vstrImageFilenamesRGB[ni],maskOut);
+        }
+
+        double ttrack= std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1).count();
+
+        vTimesTrack[ni]=ttrack;
+
+        // Wait to load the next frame
+        double T=0;
+        if(ni<nImages-1)
+            T = vTimestamps[ni+1]-tframe;
+        else if(ni>0)
+            T = tframe-vTimestamps[ni-1];
+
+        if(ttrack<T)
+            usleep((T-ttrack)*1e6);
+    }
+
+    // Stop all threads
+    SLAM.Shutdown();
+
+    // Tracking time statistics
+    sort(vTimesTrack.begin(),vTimesTrack.end());
+    float totaltime = 0;
+    for(int ni=0; ni<nImages; ni++)
+    {
+        totaltime+=vTimesTrack[ni];
+    }
+    cout << "-------" << endl << endl;
+    cout << "median tracking time: " << vTimesTrack[nImages/2] << endl;
+    cout << "mean tracking time: " << totaltime/nImages << endl;
+
+    // Save camera trajectory
+    SLAM.SaveTrajectoryTUM("CameraTrajectory.txt");
+    SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
+
+    return 0;
+}
+
+void getMask(const cv::Mat & maskRCNN, cv::Mat &mask, cv::Mat &maskColor)
+{
+    cv::Scalar color(139,0,0);
+    for(int i=0;i<maskRCNN.rows;i++)
+    {
+        for(int j=0;j<maskRCNN.cols;j++)
+        {
+            if(maskRCNN.at<uchar>(i,j)==0)
+                continue;
+            else
+            {
+                mask.at<uchar>(i,j)=1;
+                if(maskRCNN.at<uchar>(i,j)==1)
+                {
+                    maskColor.at<cv::Vec3b>(i,j)[0]=color[0];
+                    maskColor.at<cv::Vec3b>(i,j)[1]=color[1];
+                    maskColor.at<cv::Vec3b>(i,j)[2]=color[2];
+                }
+                if(maskRCNN.at<uchar>(i,j)==2)
+                {
+                    maskColor.at<cv::Vec3b>(i,j)[0]=0;
+                    maskColor.at<cv::Vec3b>(i,j)[1]=128;
+                    maskColor.at<cv::Vec3b>(i,j)[2]=0;
+                }
+                if(maskRCNN.at<uchar>(i,j)==3)
+                {
+                    maskColor.at<cv::Vec3b>(i,j)[0]=0;
+                    maskColor.at<cv::Vec3b>(i,j)[1]=0;
+                    maskColor.at<cv::Vec3b>(i,j)[2]=128;
+                }
+            }
+        }
+    }
+}
+
+void LoadImages(const string &strAssociationFilename, vector<string> &vstrImageFilenamesRGB,
+                vector<string> &vstrImageFilenamesD, vector<double> &vTimestamps)
+{
+    ifstream fAssociation;
+    fAssociation.open(strAssociationFilename.c_str());
+    while(!fAssociation.eof())
+    {
+        string s;
+        getline(fAssociation,s);
+        if(!s.empty())
+        {
+            stringstream ss;
+            ss << s;
+            double t;
+            string sRGB, sD;
+            ss >> t;
+            vTimestamps.push_back(t);
+            ss >> sRGB;
+            vstrImageFilenamesRGB.push_back(sRGB);
+            ss >> t;
+            ss >> sD;
+            vstrImageFilenamesD.push_back(sD);
+
+        }
+    }
+}
---
+++ src/python/MaskRCNN.py
@@ -0,0 +1,169 @@
+import os
+import sys
+import random
+import math
+import numpy as np
+import skimage.io
+import matplotlib
+import matplotlib.pyplot as plt
+
+import coco
+import utils
+import model as modellib
+# import visualize
+
+class Mask:
+    """
+    """
+    def __init__(self):
+        print 'Initializing Mask RCNN network...'
+	# Root directory of the project
+	ROOT_DIR = os.getcwd()
+	ROOT_DIR = "./src/python"
+	print(ROOT_DIR)
+
+	# Directory to save logs and trained model
+	MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+	# Path to trained weights file
+	COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+
+	# Set batch size to 1 since we'll be running inference on
+    	# one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+
+	class InferenceConfig(coco.CocoConfig):
+	    GPU_COUNT = 1
+	    IMAGES_PER_GPU = 1
+
+	config = InferenceConfig()
+	config.display()
+
+
+	# Create model object in inference mode.
+	self.model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+
+	# Load weights trained on MS-COCO
+	self.model.load_weights(COCO_MODEL_PATH, by_name=True)
+	self.class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+               'bus', 'train', 'truck', 'boat', 'traffic light',
+               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+               'kite', 'baseball bat', 'baseball glove', 'skateboard',
+               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+               'teddy bear', 'hair drier', 'toothbrush']
+	self.class_colors = {'BG':0., 'person':1., 'bicycle':0., 'car':0., 'motorcycle':0., 'airplane':0.,
+       'bus':0., 'train':0., 'truck':0., 'boat':0., 'traffic light':0.,
+       'fire hydrant':0., 'stop sign':0., 'parking meter':0., 'bench':0., 'bird':0.,
+       'cat':0., 'dog':0., 'horse':0., 'sheep':0., 'cow':0., 'elephant':0., 'bear':0.,
+       'zebra':0., 'giraffe':0., 'backpack':0., 'umbrella':0., 'handbag':0., 'tie':0.,
+       'suitcase':0., 'frisbee':0., 'skis':0., 'snowboard':0., 'sports ball':0.,
+       'kite':0., 'baseball bat':0., 'baseball glove':0., 'skateboard':0.,
+       'surfboard':0., 'tennis racket':0., 'bottle':0., 'wine glass':0., 'cup':0.,
+       'fork':0., 'knife':0., 'spoon':0., 'bowl':0., 'banana':0., 'apple':0.,
+       'sandwich':0., 'orange':0., 'broccoli':0., 'carrot':0., 'hot dog':0., 'pizza':0.,
+       'donut':0., 'cake':0., 'chair':0., 'couch':0., 'potted plant':0., 'bed':0.,
+       'dining table':0., 'toilet':0., 'tv':3., 'laptop':0., 'mouse':0., 'remote':0.,
+       'keyboard':2., 'cell phone':0., 'microwave':0., 'oven':0., 'toaster':0.,
+       'sink':0., 'refrigerator':0., 'book':0., 'clock':0., 'vase':0., 'scissors':0.,
+       'teddy bear':0., 'hair drier':0., 'toothbrush':0.}
+    print 'Initialated Mask RCNN network...'
+
+    def GetDynSeg(self,image,image2=None):
+	h = image.shape[0]
+	w = image.shape[1]
+	if len(image.shape) == 2:
+		im = np.zeros((h,w,3))
+		im[:,:,0]=image
+		im[:,:,1]=image
+		im[:,:,2]=image
+		image = im
+	#if image2 is not None:
+	#	args+=[image2]
+	# Run detection
+	results = self.model.detect([image], verbose=0)
+	# Visualize results
+	r = results[0]
+	i = 0
+	mask = np.zeros((h,w))
+	for roi in r['rois']:
+    		image_m = r['masks'][:,:,i]		
+    		mask[image_m == 1] = self.class_colors[self.class_names[r['class_ids'][i]]]
+		#print self.class_colors[self.class_names[r['class_ids'][i]]]
+		'''
+		if self.class_names[r['class_ids'][i]] == 'person':
+			image_m = r['masks'][:,:,i]
+			mask[image_m == 1] = 1.
+		if self.class_names[r['class_ids'][i]] == 'bicycle':
+			image_m = r['masks'][:,:,i]
+			mask[image_m == 1] = 1.
+		if self.class_names[r['class_ids'][i]] == 'car':
+			image_m = r['masks'][:,:,i]
+			mask[image_m == 1] = 1.
+		if self.class_names[r['class_ids'][i]] == 'motorcycle':
+			image_m = r['masks'][:,:,i]
+			mask[image_m == 1] = 1.
+		if self.class_names[r['class_ids'][i]] == 'airplane':
+			image_m = r['masks'][:,:,i]
+			mask[image_m == 1] = 1.
+		if self.class_names[r['class_ids'][i]] == 'bus':
+			image_m = r['masks'][:,:,i]
+			mask[image_m == 1] = 1.
+		if self.class_names[r['class_ids'][i]] == 'train':
+			image_m = r['masks'][:,:,i]
+			mask[image_m == 1] = 1.
+		if self.class_names[r['class_ids'][i]] == 'truck':
+			image_m = r['masks'][:,:,i]
+			mask[image_m == 1] = 1.
+		if self.class_names[r['class_ids'][i]] == 'boat':
+			image_m = r['masks'][:,:,i]
+			mask[image_m == 1] = 1.
+		if self.class_names[r['class_ids'][i]] == 'bird':
+			image_m = r['masks'][:,:,i]
+			mask[image_m == 1] = 1.
+		if self.class_names[r['class_ids'][i]] == 'cat':
+			image_m = r['masks'][:,:,i]
+			mask[image_m == 1] = 1.
+		if self.class_names[r['class_ids'][i]] == 'dog':
+			image_m = r['masks'][:,:,i]
+			mask[image_m == 1] = 1.
+		if self.class_names[r['class_ids'][i]] == 'horse':
+			image_m = r['masks'][:,:,i]
+			mask[image_m == 1] = 1.
+		if self.class_names[r['class_ids'][i]] == 'sheep':
+			image_m = r['masks'][:,:,i]
+			mask[image_m == 1] = 1.
+		if self.class_names[r['class_ids'][i]] == 'cow':
+			image_m = r['masks'][:,:,i]
+			mask[image_m == 1] = 1.
+		if self.class_names[r['class_ids'][i]] == 'elephant':
+			image_m = r['masks'][:,:,i]
+			mask[image_m == 1] = 1.
+		if self.class_names[r['class_ids'][i]] == 'bear':
+			image_m = r['masks'][:,:,i]
+			mask[image_m == 1] = 1.
+		if self.class_names[r['class_ids'][i]] == 'zebra':
+			image_m = r['masks'][:,:,i]
+			mask[image_m == 1] = 1.
+		if self.class_names[r['class_ids'][i]] == 'giraffe':
+			image_m = r['masks'][:,:,i]
+			mask[image_m == 1] = 1.		
+		'''
+		i+=1
+	#print('GetSeg mask shape:',mask.shape)
+
+	return mask
+
+    
+	
+
+    
+
+
